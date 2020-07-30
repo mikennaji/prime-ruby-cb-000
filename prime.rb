8dfc9c772c44191cfcdd2fe1d23d07578cf2b1a7
Michael Nnaji <mnnaji3@uwo.ca>
2020-07-30 21:28:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,4 +6,13 @@ def prime?(number1)
       return true
     end
   end
+ not_some=   range1.any?{ |number| number1 % number != 0 }
+  if not_some === false && number1 < 0
+    puts "Be sure to account for negative numbers!"
+    return false
+  end
+  if not_some===false
+    return false
+ end
+  
 end
