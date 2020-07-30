7a147f16a5ba8a4473c4e465050104b2ec156ab7
Michael Nnaji <mnnaji3@uwo.ca>
2020-07-30 21:31:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,7 @@
-# Add  code here!
-def prime?(number1)
-  range1 =(-1..1000).to_a
-  range1.each do |number|
-    if number1 % number === 0
-      return true
-    end
+def prime?(num)
+  unless num.abs == 1 || num.abs == 0
+    (2...num.abs).detect { |x| num % x == 0 }.nil?
+  else
+    false
   end
- not_some=   range1.any?{ |number| number1 % number != 0 }
-  if not_some === false && number1 < 0
-    puts "Be sure to account for negative numbers!"
-    return false
-  end
-  if not_some===false
-    return false
- end
-
 end
